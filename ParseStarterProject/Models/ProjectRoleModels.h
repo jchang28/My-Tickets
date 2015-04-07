d6f751b9c6ac777397a66d1ddcf23188979c7b22
Jeff@Level3 Mac Book Pro <jeff+github@level3apps.com>
2015-04-07 14:08:42 -0700

    Added roles constants for easy access to roles.

---
+++ ParseStarterProject/Models/ProjectRoleModels.h
@@ -0,0 +1,16 @@
+//
+//  ProjectRoleModels.h
+//  My Tickets
+//
+//  Created by Jeff@Level3 on 4/7/15.
+//
+//
+
+#ifndef My_Tickets_ProjectRoleModels_h
+#define My_Tickets_ProjectRoleModels_h
+
+static NSString * const MTParseProjectRoleMemberPostfix = @"Member";
+static NSString * const MTParseProjectRoleMetaPostfix = @"Meta";
+static NSString * const MTParseProjectRoleAdminPostfix = @"Admin";
+
+#endif
